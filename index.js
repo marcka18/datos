00862ffba4ba876a65a888fc122c8f4a144d5669
Marck Alvarez <>
2021-04-27 21:35:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var nombre=prompt('ingrese su nombre');
+var edad=prompt('ingrese su edad');
+var hobbie=prompt('ingrese su hobbie');
+console.log('mi nombre es: ' + nombre);
+console.log('mi edad es: ' + edad);
+console.log('mi hobbie es: ' + hobbie);
+console.log('me gusta programar ');
